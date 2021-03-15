3caa7e00978a9c6c3b053405ef4a73dec3023516
llenaX <jaimitr14@gmail.com>
2021-03-15 16:34:15 +0300

    djs-docs

---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const DiscordJS = require("discord.js");
+
+const cInstaller = require("c-installer");
+
+const discord_client = new DiscordJS.Client();
+
+const ci_client = new cInstaller.Client({ discord_client })
+
+ci_client.discord.load(async function docs(req) {
+const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+    req.args
+    )}`;
+    await axios
+    .get(url)
+    .then((t) => req.msg.channel.send({ embed: t.data }))
+    .catch(() => {
+        req.msg.channel.send(
+        `${req.args} couldn't be found at discord.js documentation.`
+        );
+    });
+})
+
+discord_client.login(process.env.DC_TOKEN)
