3e79542d7f8116d2c94da1732d0c01300a16b8dd
llenaX <jaimitr14@gmail.com>
2021-03-20 23:00:46 +0300

    c-installer update

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const axios = require("axios");
-const DiscordJS = require("discord.js");
 
-const cInstaller = require("c-installer");
+const { Handler } = require("c-installer");
 
-const discord_client = new DiscordJS.Client();
+const commands = new Handler();
 
-const ci_client = new cInstaller.Client({ client: discord_client })
-
-ci_client.discord.load(async function docs(req) {
-const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
+const docs = async (req) => {
+  const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(
     req.args
-    )}`;
-    await axios
+  )}`;
+  await axios
     .get(url)
     .then((t) => req.msg.channel.send({ embed: t.data }))
     .catch(() => {
-        req.msg.channel.send(
+      req.msg.channel.send(
         `${req.args} couldn't found at discord.js documentation.`
-        );
+      );
     });
-})
+};
 
-discord_client.login(process.env.DC_TOKEN)
+commands.loadCommand(docs);
+
+commands.login();
